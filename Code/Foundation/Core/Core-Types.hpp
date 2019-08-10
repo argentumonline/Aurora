9f6efaf8adf09bf7cdab575404e7fabddde8fa67
Wolftein <agustin.l.alvarez.1@gmail.com>
2019-08-09 22:28:35 -0300

    Added initial files.
    Signed-off-by: Wolftein <wolftein1@gmail.com>

---
+++ Code/Foundation/Core/Core-Types.hpp
@@ -0,0 +1,55 @@
+// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+// Copyright (C) 2019 by Agustin Alvarez <agustin.l.alvarez.1@gmail.com>. All rights reserved.
+//
+// This work is licensed under the terms of the Apache License, Version 2.0.
+//
+// See <https://raw.githubusercontent.com/argentumonline/Aurora/master/LICENSE>.
+// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+#pragma once
+
+// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+// [  HEADER  ]
+// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+#include <cstdint>
+
+// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+// [   CODE   ]
+// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+inline namespace STL
+{
+    /// \see bool
+    using Bool   = bool;
+
+    /// \see float
+    using Float  = float;
+
+    /// \see double
+    using Double = double;
+
+    /// \see std::int8_t
+    using SInt8  = std::int8_t;
+
+    /// \see std::int16_t
+    using SInt16 = std::int16_t;
+
+    /// \see std::int32_t
+    using SInt32 = std::int32_t;
+
+    /// \see std::int64_t
+    using SInt64 = std::int64_t;
+
+    /// \see std::uint8_t
+    using UInt8  = std::uint8_t;
+
+    /// \see std::uint16_t
+    using UInt16 = std::uint16_t;
+
+    /// \see std::uint32_t
+    using UInt32 = std::uint32_t;
+
+    /// \see std::uint64_t
+    using UInt64 = std::uint64_t;
+}
---
+++ Code/Foundation/Graphic/Graphic-Pipeline.cpp
@@ -0,0 +1,13 @@
+// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+// Copyright (C) 2019 by Agustin Alvarez <agustin.l.alvarez.1@gmail.com>. All rights reserved.
+//
+// This work is licensed under the terms of the Apache License, Version 2.0.
+//
+// See <https://raw.githubusercontent.com/argentumonline/Aurora/master/LICENSE>.
+// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+// [  HEADER  ]
+// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+#include "Graphic-Pipeline.hpp"
